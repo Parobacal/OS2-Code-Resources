6776ae72630bc9e50866fc269b3039cf19678bb1
Pablo Barillas <parobacal@gmail.com>
2022-02-02 14:50:24 -0600

    update: licence added.

--- part1/modules/basic_mod/basic_mod.c
+++ part1/modules/basic_mod/basic_mod.c
@@ -1,6 +1,11 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
+MODULE_LICENSE(“GPL”);
+MODULE_AUTHOR(“parobacal”);
+MODULE_DESCRIPTION(“Basic Linux module.”);
+MODULE_VERSION(“0.01”);
+
 static int __init load_module(void) {
     printk(KERN_INFO "Hello world!\n");
     return 0;
