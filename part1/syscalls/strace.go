9a56303a57745600a198cd65bb4674167430d0c5
Pablo Barillas <parobacal@gmail.com>
2022-02-07 14:32:43 -0600

    add

--- part1/syscalls/strace.go
+++ part1/syscalls/strace.go
@@ -5,7 +5,46 @@ import (
     "os"
     "os/exec"
     "syscall"
+"text/tabwriter"
+
+    "github.com/seccomp/libseccomp-golang"
+
 )
+
+type syscallCounter []int
+
+const maxSyscalls = 303
+
+func (s syscallCounter) init() syscallCounter {
+    s = make(syscallCounter, maxSyscalls)
+    return s
+}
+
+func (s syscallCounter) inc(syscallID uint64) error {
+    if syscallID > maxSyscalls {
+        return fmt.Errorf("invalid syscall ID (%x)", syscallID)
+    }
+
+    s[syscallID]++
+    return nil
+}
+
+func (s syscallCounter) print() {
+    w := tabwriter.NewWriter(os.Stdout, 0, 0, 8, ' ', tabwriter.AlignRight|tabwriter.Debug)
+    for k, v := range s {
+        if v > 0 {
+            name, _ := seccomp.ScmpSyscall(k).GetName()
+            fmt.Fprintf(w, "%d\t%s\n", v, name)
+        }
+    }
+    w.Flush()
+}
+
+func (s syscallCounter) getName(syscallID uint64) string {
+    name, _ := seccomp.ScmpSyscall(syscallID).GetName()
+    return name
+}
+
 
 func main() {
     var err error
@@ -48,3 +87,4 @@ func main() {
 
     ss.print()
 }
+
