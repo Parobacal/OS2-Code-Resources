835cd438b613a48d748cffceb085b3e9e9c1744b
Pablo Barillas <parobacal@gmail.com>
2022-02-07 14:22:47 -0600

    test

--- part1/syscalls/strace.go
+++ part1/syscalls/strace.go
@@ -1,62 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-	"syscall"
+    "fmt"
+    "os"
+    "os/exec"
+    "syscall"
 )
 
 func main() {
-	var err error
-	var regs syscall.PtraceRegs
-	var ss syscallCounter
-	ss = ss.init()
+    var err error
+    var regs syscall.PtraceRegs
+    var ss syscallCounter
+    ss = ss.init()
+    pid := 8363
+    exit := true
 
-	fmt.Println("Run: ", os.Args[1:])
+    for {
+        if exit {
+            err = syscall.PtraceGetRegs(pid, &regs)
+            if err != nil {
+                break
+            }
+            //fmt.Printf("%#v \n",regs)
+            name := ss.getName(regs.Orig_rax)
+            fmt.Printf("name: %s, id: %d \n", name, regs.Orig_rax)
+            ss.inc(regs.Orig_rax)
+        }
 
-	cmd := exec.Command(os.Args[1], os.Args[2:]...)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	cmd.Stdin = os.Stdin
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Ptrace: true,
-	}
+        /**
+        http://www.linuxjournal.com/article/6100?page=0,1
+        Here we are tracing the write system calls, and ls makes three write system calls. The call to ptrace, with a first argument of PTRACE_SYSCALL, makes the kernel stop the child process whenever a system call entry or exit is made. It's equivalent to doing a PTRACE_CONT and stopping at the next system call entry/exit.
+        */
+        err = syscall.PtraceSyscall(pid, 0)
+        if err != nil {
+            panic(err)
+        }
 
-	cmd.Start()
-	err = cmd.Wait()
-	if err != nil {
-		fmt.Printf("Wait err %v\n", err)
-	}
+        // http://www.linuxjournal.com/article/6100?page=0,1
+        //The status variable in the wait call is used to check whether the child has exited. This is the typical way to check whether the child has been stopped by ptrace or was able to exit.
+        _, err = syscall.Wait4(pid, nil, 0, nil)
+        if err != nil {
+            panic(err)
+        }
 
-	pid := cmd.Process.Pid
-	exit := true
+        exit = !exit
+    }
 
-	for {
-		//记得 PTRACE_SYSCALL 会在进入和退出syscall时使 tracee 暂停，所以这里用一个变量控制，RAX的内容只打印一遍
-		if exit {
-			err = syscall.PtraceGetRegs(pid, &regs)
-			if err != nil {
-				break
-			}
-			//fmt.Printf("%#v\n",regs)
-			name := ss.getName(regs.Orig_rax)
-			fmt.Printf("name: %s, id: %d\n", name, regs.Orig_rax)
-			ss.inc(regs.Orig_rax)
-		}
-		//上面Ptrace有提到的一个request命令
-		err = syscall.PtraceSyscall(pid, 0)
-		if err != nil {
-			panic(err)
-		}
-		//猜测是等待进程进入下一个stop，这里如果不等待，那么会打印大量重复的调用函数名
-		_, err = syscall.Wait4(pid, nil, 0, nil)
-		if err != nil {
-			panic(err)
-		}
-
-		exit = !exit
-	}
-
-	ss.print()
+    ss.print()
 }
