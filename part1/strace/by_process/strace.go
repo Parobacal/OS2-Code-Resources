b2d9a2839d8bc614ba925feb14bc77350ddbe484
Pablo Barillas <parobacal@gmail.com>
2022-02-17 15:32:28 -0600

    update

---
+++ part1/strace/by_process/strace.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"syscall"
+)
+
+func main() {
+	var err error
+	var regs syscall.PtraceRegs
+	var ss syscallCounter
+	ss = ss.init()
+
+	var pid = 1
+	exit := true
+
+	erx := syscall.PtraceAttach(pid)
+	if err != nil {
+		fmt.Print("Attach")
+		fmt.Print(erx)
+	}
+
+	err = syscall.PtraceSetOptions(pid, syscall.PTRACE_O_TRACESYSGOOD)
+	if err != nil {
+		fmt.Println("Ptrace set options")
+		panic(err)
+	}
+
+	for {
+		if exit {
+			err = syscall.PtraceGetRegs(pid, &regs)
+			if err != nil {
+				break
+			}
+			name := ss.getName(regs.Orig_rax)
+			fmt.Printf("name: %s, id: %d \n", name, regs.Orig_rax)
+			ss.inc(regs.Orig_rax)
+		}
+
+		err = syscall.PtraceSyscall(pid, 0)
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = syscall.Wait4(pid, nil, 0, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		exit = !exit
+	}
+
+	ss.print()
+}
